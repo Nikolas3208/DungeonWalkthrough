f0d81310ff0d2a9e8cb2a8c33eca173157a4abe0
Николай Гастелло <gastello.nik7803@gmail.com>
2025-02-23 14:35:22 +0200

    Сортировака ассетов в AssetManager

--- Core/Content/AssetManager.cs
+++ Core/Content/AssetManager.cs
@@ -8,14 +8,14 @@ public class AssetManager
     private SortedDictionary<string, Asset> _assets;
 
     /// <summary>
-    /// Путь к папке с обектами
+    /// Путь к папке с ассетами
     /// </summary>
     private string _path;
 
     /// <summary>
     /// Екземпляр менеджера обектов
     /// </summary>
-    /// <param name="path"> Путь к обектам </param>
+    /// <param name="path"> Путь к ассетам </param>
     public AssetManager(string path)
     {
         _path = path;
@@ -23,7 +23,7 @@ public class AssetManager
     }
 
     /// <summary>
-    /// Загрузка обектов
+    /// Загрузка ассутов
     /// </summary>
     public void LoadAssets()
     {
@@ -47,7 +47,7 @@ public class AssetManager
     /// <summary>
     /// Получить обект
     /// </summary>
-    /// <param name="name"> Имя обекта </param>
+    /// <param name="name"> Имя ассота </param>
     /// <returns> Обект, null если имени нет в списке </returns>
     public Asset? GetAsset(string name)
     {
@@ -61,7 +61,7 @@ public class AssetManager
     /// <summary>
     /// Получить обект и преобразовать тип
     /// </summary>
-    /// <typeparam name="T"> Тип обекта </typeparam>
+    /// <typeparam name="T"> Тип ассота </typeparam>
     /// <param name="name"> Имя обекта </param>
     /// <returns> Обект, если имени нет в списке null </returns>
     public T? GetAsset<T>(string name) where T : Asset
@@ -70,5 +70,60 @@ public class AssetManager
             return null;
 
         return (T)_assets[name];
+    }
+
+    /// <summary>
+    /// Получить сортированый массив обекто
+    /// </summary>
+    /// <param name="type"> Тип ассота </param>
+    /// <param name="sort"> Имя </param>
+    /// <returns> Сортированый списов по имени и типу </returns>
+    public List<Asset>? GetSortAssets(AssetType type = AssetType.None, string sort = "")
+    {
+        List<Asset> assets = new List<Asset>();
+
+        if (type != AssetType.None)
+        {
+            assets = _assets.Values.Where(a => a.Type == type).ToList();
+        }
+
+        if (sort != "" && assets != null)
+        {
+            assets = assets.Where(a => a.Name.ToLower().Contains(sort.ToLower())).ToList();
+        }
+
+        return assets;
+    }
+
+    /// <summary>
+    /// Получить сортированый список 
+    /// </summary>
+    /// <typeparam name="T"> Тип екземпляра обекта </typeparam>
+    /// <param name="type"> Тип ассота </param>
+    /// <param name="sort"> Имя </param>
+    /// <returns> Сортированый список по имени и типу приведеннй к типу </returns>
+    public List<T>? GetSortAssets<T>(AssetType type = AssetType.None, string sort = "") where T : Asset
+    {
+        List<T> assets = new List<T>();
+        if (type != AssetType.None)
+        {
+            assets = _assets.Values.Where(a => a.Type == type).Cast<T>().ToList();
+        }
+
+        if (sort != "" && assets != null)
+        {
+            assets = assets.Where(a => a.Name.ToLower().Contains(sort.ToLower())).ToList();
+        }
+
+        return assets;
+    }
+
+    /// <summary>
+    /// Список всех ассотов
+    /// </summary>
+    /// <returns> Список ассетов </returns>
+    public List<Asset> GetAssets()
+    {
+        return _assets.Values.ToList();
     }
 }
